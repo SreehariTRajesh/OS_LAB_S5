34a8ab05648ebf3d45dc55c96e45575b09a2a8db
Sreehari T Rajesh <sreehari_b200754cs@nitc.ac.in>
2022-11-09 03:02:04 +0530

    stage27 swapper daemons implemented

---
+++ stage27/compile.sh
@@ -0,0 +1,54 @@
+./spl ../stage26/os_startup.spl
+echo "Successful1"
+./spl ../stage26/scheduler_module.spl
+echo "Successful2"
+./spl ../stage26/boot_module.spl
+echo "Successful3"
+./spl ../stage26/sample_int7.spl
+echo "Successful4"
+./spl ../stage26/sample_timer.spl
+echo "Successful5"
+./spl ../stage26/int10.spl
+echo "Successful6"
+./spl ../stage26/module_0.spl
+echo "Successful7"
+./spl ../stage26/module_1.spl
+echo "Successful8"
+./spl ../stage26/module_2.spl
+echo "Successful9"
+./spl ../stage26/module_4.spl
+echo "Successful10"
+./spl ../stage26/int6_mod.spl
+echo "Successful11"
+./spl ../stage26/console_interrupt.spl
+echo "Successful12"
+./spl ../stage26/sample_int9.spl
+echo "Successful13"
+./spl ../stage26/disk_interrupt.spl
+echo "Successful14"
+./spl ../stage26/exhandler.spl
+echo "Successful15"
+./spl ../stage26/sample_int8.spl
+echo "Successful16"
+./spl ../stage26/sample_int11.spl
+echo "Successful17"
+./spl ../stage26/sample_int15.spl
+echo "Successful18"
+./spl ../stage26/sample_int13_copy.spl
+echo "Successful19"
+./spl ../stage26/sample_int14_copy.spl
+echo "Successful20"
+./spl ../stage26/sample_int4.spl
+echo "Successful21"
+./spl ../stage26/module_3.spl
+echo "Successful22"
+./spl ../stage26/sample_int5.spl
+echo "Successful23"
+./spl ../stage26/int7_mod.spl
+echo "Successful 24"
+./spl ../stage26/sample_int12.spl
+echo "Successful 25"
+./spl ../stage26/sample_int16.spl
+echo "Successful 26"
+./spl ../stage26/sample_int17.spl
+echo "Successful 27"
